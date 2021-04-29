fe543bffd4914f4c09279ff643b6838f2d531e61
Fakery Bakery <76186054+fakerybakery@users.noreply.github.com>
2021-04-29 18:13:39 -0500

    Create install.sql

---
+++ dist/link/install.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `links` (
+  `url` varchar(255) DEFAULT NULL,
+  `dateofvisit` varchar(255) DEFAULT NULL,
+  `clientip` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
